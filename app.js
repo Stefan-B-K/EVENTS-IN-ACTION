647ccccf711294279e1d3adae8929e6ed2d1d33c
iStef <stefan_b_k@icloud.com>
2022-05-30 14:30:57 +0300

    finish

--- app.js
+++ app.js
@@ -2,20 +2,33 @@ const app = Vue.createApp({
   data () {
     return {
       counter: 0,
-      name: '',
+      firstName: '',
+      lastName: ''
+    }
+  },
+  watch: {
+    counter (value) {
+      if (value > 10)  this.counter = 0
+    }
+  },
+  computed: {
+    fullName () {
+      return this.firstName ? this.firstName + ' ' + this.lastName : ''
     }
   },
   methods: {
     add (number) {
       this.counter += number
     },
-    remove (number) {
+    subtract (number) {
       this.counter -= number
     },
     resetInput () {
-      this.name = ''
+      this.firstName = ''
+      this.lastName = ''
     }
   }
 })
 
 app.mount('#events')
+
